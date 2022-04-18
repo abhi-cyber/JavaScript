eaaf474c35a689b43a29c7e9ca78190a66bdab7c
abhi-cyber <abhirajchatrath@gmail.com>
2022-04-18 20:44:02 +0530

    Arrays | Function

--- DAY-2/app.js
+++ DAY-2/app.js
@@ -6,3 +6,15 @@ let boolean = true;
 let unDefined;
 let nullVar = null;
 
+
+// Arrays - It is of heterogeneous type.
+
+let arr = [1,2,3,4,5,6,7,8,9,10, "Hello"]
+
+// for( let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+console.log("This is an array: "+arr);
+console.log("These are the DataTypes: "+string, integer, boolean, unDefined, nullVar);
+
