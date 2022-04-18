6522e1c19123d3c0af48f40029d001f0057a5da7
abhi-cyber <abhirajchatrath@gmail.com>
2022-04-18 20:44:35 +0530

    Operators (Note)

--- DAY-2/app.js
+++ DAY-2/app.js
@@ -18,3 +18,27 @@ let arr = [1,2,3,4,5,6,7,8,9,10, "Hello"]
 console.log("This is an array: "+arr);
 console.log("These are the DataTypes: "+string, integer, boolean, unDefined, nullVar);
 
+/* 
+    operators
+            - Artihmetic Operator ==> +, -, /, %, *
+            - Assigment Operator ==> =
+            - Comparison Operator==> ==, >=, <=
+            - Logical Operators ==> and[&&], or[||], not[!]
+
+                                                            AND
+                                                                |-----|-----|
+                                                                | T F |  F  |
+                                                                | F F |  F  |
+                                                                | T T |  T  |
+                                                                | F T |  F  |
+                                                            OR
+                                                                |-----|-----|
+                                                                | T F |  T  |
+                                                                | F F |  F  |
+                                                                | T T |  T  |
+                                                                | F T |  T  |
+
+
+    Precendence: PEMDAS
+    Associativity is of ASSIGNMENT OPERTORS
+*/
