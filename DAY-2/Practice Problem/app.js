ca37917070f646bcd694965cd742c65283bdea78
abhi-cyber <abhirajchatrath@gmail.com>
2022-04-18 22:19:22 +0530

    Function for Grades

--- DAY-2/Practice Problem/app.js
+++ DAY-2/Practice Problem/app.js
@@ -10,3 +10,26 @@ function swap(n1, n2){
 
 swap(25,35);
 
+// Without Third Variable
+
+function swapWithoutThird(n1, n2){
+    n1 = n1 + n2;
+    n2 = n1 - n2;
+    n1 = n1 - n2;
+    console.log(n1, n2);
+}
+swapWithoutThird(25,35);
+
+// Print the Grade (a b c)
+
+function grade(n1, n2, n3){
+    let marks = (n1 + n2 + n3)/3
+    if(marks<= 100 && marks >= 85){
+        console.log('Grade A');
+    }else if(marks < 85 && marks >= 60){
+        console.log('Grade B');
+    }else if(marks < 60){
+        console.log('Grade C');
+    }
+}
+grade(85, 25, 55)
