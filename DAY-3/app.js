857eb8d8ed411b13a63b3a4be21f9f7b57b4efc5
abhi-cyber <abhirajchatrath@gmail.com>
2022-04-26 13:34:57 +0530

    Func Notes

---
+++ DAY-3/app.js
@@ -0,0 +1,19 @@
+/*
+    Every function has a scope ==> Only the code defined in function is executed;
+
+    function <function name>([arguements]){
+        {SCOPE OF FUNCTION}
+    }
+
+    Types: 
+        1. Without Argument Without Return Value
+        2. Without Argument With Return Value
+        3. With Argument Without Return Value
+        4. With Argument With Return Value
+
+    Functions can be defined anywhere;
+
+    IF we have more than one functions with same name then they are differntiated on the basis of the number of passed arguemnts.
+    IF the number of passed arguments is same then the data type of the arguments is checked.
+
+*/
